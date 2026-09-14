6bb7e490bff6d35ab049acebdbd4f02e227fdced
noobiwow <valentin.millet@efrei.net>
2026-09-14 23:23:26 +0900

    Update Zhen

--- modules/php/Cards/StarterDecks/or_65_zhen.inc.php
+++ modules/php/Cards/StarterDecks/or_65_zhen.inc.php
@@ -1,7 +1,7 @@
 <?php
 const STARTER_ZHEN = [
   'OD_Common_ZhenZephyr' => 1,
-  'OD_Common_TheOrigamium' => 2,
+  'OD_Common_CorruptedGargoyle' => 2,
   'OD_Rare_AscendedTrooper' => 3,
   'OD_Common_Gargoyle' => 2,
   'OD_Rare_OrdisMessenger' => 3,
@@ -13,9 +13,9 @@ const STARTER_ZHEN = [
   'OD_Common_LicenseWithdrawal' => 2,
   'OD_Common_Soar' => 3,
   'OD_Common_TheFrogPrince' => 2,
-  'OD_Common_MonolithLegate' => 2,
   'OD_Rare_HavenWarrior' => 2,
-  'OD_Rare_OutofaClearSky' => 2,
+  'OD_Exalted_EvacuatetheCity' => 2,
   'OD_Common_TheUglyDuckling' => 3,
+  'OD_Rare_NilsHolgersson' => 2,
 ];
 ?>
